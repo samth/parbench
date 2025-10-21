844467057e4d106661d1042921c3c028991a03d9
Sam Tobin-Hochstadt <samth@indiana.edu>
2025-10-21 12:12:55 -0400

    Fix deps.

--- info.rkt
+++ info.rkt
@@ -2,6 +2,7 @@
 
 (define collection "parbench")
 (define version "0.1")
-(define deps '("base"
+(define deps '("plot-gui-lib"
+               "base"
                "plot-lib"))
 (define build-deps '("rackunit-lib"))
